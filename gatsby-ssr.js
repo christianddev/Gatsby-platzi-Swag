75855787a2cd8ebfbe0762e73d04670b581ddc48
Christian David G <cdavid.ggarcia@gmail.com>
2022-07-22 07:08:20 +0200

    feat: check name files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
 const React = require("react")
 const { CartProvider } = require("./src/context")
 const { GlobalStyles } = require("./src/styles")
-const Layout = require("./src/components/layout").default
+const Layout = require("./src/components/Layout").default
 
 exports.wrapPageElement = ({ element }) => (
   <CartProvider>
